f09c9bf1e1a2f009936f768949ba41f508c0e176
athoo55 <dhwdhwra@gmail.com>
2024-02-14 23:07:34 +0300

    add commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(allstudents) {
+    var minStudent = allstudents[0];
+    for (let i = 0; i < allstudents.lenght; i++) {
+        if (allstudents[i].age < minStudent.age)
+            minStudent = allstudents[i];
 
-module.exports = showYoungestStudent;
+    }
+    console.log("the youngest student is:", minStudent.name);
+}
+let allstudents = [
+    { name: "atho", age: 20 },
+    { name: "aya", age: 23 },
+    { name: "noor", age: 26 },
+    { name: "zoz", age: 22 }
+]
+showYoungestStudent(allstudents);
